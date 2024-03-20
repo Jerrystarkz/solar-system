b8bce3d4a83f04ff6c7f085894c67d5dd8df9719
Jerrystarkz <jerrynuhu99@gmail.com>
2024-03-20 08:53:49 +0100

    add: deploy workflow

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+const App = ()=> {
+  
+
+  return (
+      <div className="body">
+            <div classNameName="container">
+                  <div className="sun"><div className="font">sun</div></div>
+                  <div className="earth"><div className="font">earth</div>
+                      <div className="moon"></div>
+                  </div>
+          </div>
+    </div>
+  
+  
+  )
+}
+
+export default App
